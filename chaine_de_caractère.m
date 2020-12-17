8c7e655efa5d5c6360a704f3cf9e94df253c1a7c
Zoé Jéquier <zoe.jequier@epfl.ch>
2020-12-17 21:35:32 +0100

    voici un test

---
+++ chaine_de_caractère.m
@@ -0,0 +1,9 @@
+%chaines de caractères 
+
+%---------code ASCII-------------
+
+texte= 'hello'
+code_asci= [72, 101, 108, 108, 111]
+
+double('texte') %retourne un vecteur avec l'encodage ascii. ici : code_asci 
+char(code_asci) % retourne le texte. ici: 'hello'
